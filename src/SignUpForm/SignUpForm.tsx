dfd666c07b6e0cb42b0aa2f35d4e06a2ca9aedd6
Ho Won Cheng <ho.won.cheng@gmail.com>
2022-04-27 12:38:08 -0700

    Complete step 1

--- src/SignUpForm/SignUpForm.tsx
+++ src/SignUpForm/SignUpForm.tsx
@@ -2,6 +2,46 @@ const SignUpForm = () => {
   return (
     <div className="SignUpForm">
       <h1>Weekly Pie Sign Up</h1>
+
+      <form>
+        <label>
+          First Name
+          <input type="text" name="firstName" />
+        </label>
+
+        <label>
+          Last Name
+          <input type="text" name="lastName" />
+        </label>
+
+        <label>
+          Email
+          <input type="text" name="email" />
+        </label>
+
+        <p>Pie Size Preference</p>
+        <label>
+          <input type="radio" name="pieSizePreference" />
+          Hand Pie
+        </label>
+
+        <label>
+          <input type="radio" name="pieSizePreference" />
+          Small (6 inch diameter)
+        </label>
+
+        <label>
+          <input type="radio" name="pieSizePreference" />
+          Medium (10 inch diameter)
+        </label>
+
+        <label>
+          <input type="radio" name="pieSizePreference" />
+          Large (14 inch diameter)
+        </label>
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
