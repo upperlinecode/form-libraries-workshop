e1bf1e8493a7d2971e7fb6bd2271646982ad48a7
Ho Won Cheng <ho.won.cheng@gmail.com>
2022-04-27 15:21:09 -0700

    Add pre-styled components

--- src/App/App.styles.ts
+++ src/App/App.styles.ts
@@ -1,1 +1,13 @@
-export default {};
+import styled from "styled-components";
+
+export const Root = styled.div`
+  align-items: center;
+  background-image: url("	https://www.inspiredtaste.net/wp-content/uploads/2016/06/Blueberry-Pie-Recipe-2-1200.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+  display: flex;
+  justify-content: center;
+  min-height: 100vh;
+  min-width: 100vw;
+`;
